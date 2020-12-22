ae5aaade4e86ac2210548045720c2f720344759c
kritfencer <kritfencer@gmail.com>
2020-12-22 21:47:52 +0700

    asset

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,3 +1,7 @@
+</div>
+<script src="<?= BASEURL; ?>/js/jquery-3.5.1.slim.min.js"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.bundle.min.js"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
 </body>
 
 </html>
