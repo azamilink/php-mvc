5e97d7a9e7984c5a69f8f42423f267dd6afa6637
kritfencer <kritfencer@gmail.com>
2020-12-22 21:02:34 +0700

    views/templates

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -7,12 +7,18 @@ class about extends Controller
     $data['nama'] = $nama;
     $data['pekerjaan'] = $pekerjaan;
     $data['umur'] = $umur;
+    $data['judul'] = 'About Me';
 
+    $this->view('templates/header', $data);
     $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
 
   public function page()
   {
+    $data['judul'] = 'Page';
+    $this->view('templates/header', $data);
     $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
