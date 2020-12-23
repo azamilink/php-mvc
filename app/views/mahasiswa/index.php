38d4fccbd5788eb6e63e04459a589659c335b5f0
kritfencer <kritfencer@gmail.com>
2020-12-23 23:05:08 +0700

    insert data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,5 +1,9 @@
 <div class="row mt-4">
   <div class="col-6">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+      Tambah Data Mahasiswa
+    </button>
+    <br><br>
     <h3>Daftar Mahasiswa</h3>
     <ul class="list-group">
       <?php foreach ($data['mhs'] as $mhs) : ?>
@@ -9,5 +13,49 @@
         </li>
       <?php endforeach; ?>
     </ul>
+  </div>
+</div>
+
+<!-- modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulmodal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama">
+          </div>
+          <div class="form-group">
+            <label for="nrp">NRP</label>
+            <input type="number" class="form-control" id="nrp" name="nrp">
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email">
+          </div>
+          <div class="form-group">
+            <label for="jurusan">Jurusan</label>
+            <select class="form-control" id="jurusan" name="jurusan">
+              <option value="Teknik Informatika">Teknik Informatika</option>
+              <option value="Teknik Industri">Teknik Industri</option>
+              <option value="Teknik Pangan">Teknik Pangan</option>
+              <option value="Teknik Planologi">Teknik Planologi</option>
+              <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+            </select>
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
+      </div>
+    </div>
   </div>
 </div>
