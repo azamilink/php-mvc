ba5e6893a54c31013ab1c4be58fa3b73ee9ed4a6
kritfencer <kritfencer@gmail.com>
2020-12-25 06:07:57 +0700

    update data

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,7 +1,8 @@
 </div>
-<script src="<?= BASEURL; ?>/js/jquery-3.5.1.slim.min.js"></script>
+<script src="<?= BASEURL; ?>/js/jquery-3.2.1.min.js"></script>
 <script src="<?= BASEURL; ?>/js/bootstrap.bundle.min.js"></script>
 <script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+<script src="<?= BASEURL; ?>/js/script.js"></script>
 </body>
 
 </html>
